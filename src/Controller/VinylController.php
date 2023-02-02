a27f68e5fd70ffe85db7b6209c65f216decf55b2
Tomasz Woytkowiak <tomas.woytkowiak1505@gmail.com>
2023-02-02 17:39:42 +0100

    Created 2 simple routers and class

--- src/Controller/VinylController.php
+++ src/Controller/VinylController.php
@@ -5,18 +5,24 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use function Symfony\Component\String\u;
 
 class VinylController extends AbstractController
 {
     #[Route('/')]
-    public function homepage():Response
+    public function homepage(): Response
     {
         return new Response("Hello Symfony!!!");
     }
 
     #[Route('/browse/{gene}')]
-    public function browse(string $gene):Response
+    public function browseGen(string $gene = null): Response
     {
-        return new Response('You are browsing now: '. $gene);
+        if ($gene) {
+            $geneTitle = 'You are browsing now: ' . u(str_replace('-', ' ', $gene))->title(true);
+        } else {
+            $geneTitle= 'All Generes';
+        }
+        return new Response($geneTitle);
     }
 }
