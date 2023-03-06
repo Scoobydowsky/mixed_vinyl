7c176f06c1f2d72b53623b60ea8fb4869f0abc28
Tomasz Woytkowiak <tomas.woytkowiak1505@gmail.com>
2023-03-06 19:23:08 +0100

    dependency injection in custom service, moved some code from VinylController.php -> MixRepository.php (Chapter 12)

--- src/Service/MixRepository.php
+++ src/Service/MixRepository.php
@@ -3,14 +3,22 @@
 namespace App\Service;
 
 use Psr\Cache\CacheItemInterface;
+use Symfony\Contracts\Cache\CacheInterface;
+use Symfony\Contracts\HttpClient\HttpClientInterface;
 
 class MixRepository
 {
-    public function findAll() :array
+    public function __construct(
+        private HttpClientInterface $httpClient,
+        private CacheInterface $cache)
     {
-         return $cache->get('mixes_data', function (CacheItemInterface $cacheItem) use ($httpClient) {
+    }
+
+    public function findAll(): array
+    {
+        return $this->cache->get('mixes_data', function (CacheItemInterface $cacheItem){
             $cacheItem->expiresAfter(5);
-            $response = $httpClient->request('GET','https://raw.githubusercontent.com/SymfonyCasts/vinyl-mixes/main/mixes.json');
+            $response = $this->httpClient->request('GET', 'https://raw.githubusercontent.com/SymfonyCasts/vinyl-mixes/main/mixes.json');
 
             return $response->toArray();
         });
