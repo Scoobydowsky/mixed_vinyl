51c90fa96fe51ca1b4e69f5c16612094f5d9810b
Tomasz Woytkowiak <tomas.woytkowiak1505@gmail.com>
2023-03-13 22:28:16 +0100

    autowire service and create interactive question (chapter 25 end course 2)

--- src/Command/TalkToMeCommand.php
+++ src/Command/TalkToMeCommand.php
@@ -2,6 +2,7 @@
 
 namespace App\Command;
 
+use App\Service\MixRepository;
 use Symfony\Component\Console\Attribute\AsCommand;
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputArgument;
@@ -16,6 +17,11 @@ use Symfony\Component\Console\Style\SymfonyStyle;
 )]
 class TalkToMeCommand extends Command
 {
+    public function __construct(private MixRepository $mixRepository)
+    {
+        parent::__construct();
+    }
+
     protected function configure(): void
     {
         $this
@@ -37,6 +43,11 @@ class TalkToMeCommand extends Command
 
         $io->success($message);
 
+        if($io->confirm('Do you want mix recommendation>')){
+            $mixes = $this->mixRepository->findAll();
+            $mix = $mixes[array_rand($mixes)];
+            $io->note('I recommend the mix: '.$mix['title']);
+        }
         return Command::SUCCESS;
     }
 }
