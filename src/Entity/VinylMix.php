b6684a6421d4dfdf40c6781fd54e16236ecf68d8
Tomasz Woytkowiak <tomas.woytkowiak1505@gmail.com>
2023-04-23 11:25:09 +0200

    Clean URLs with Sluggable(chapter 18)

--- src/Entity/VinylMix.php
+++ src/Entity/VinylMix.php
@@ -5,6 +5,7 @@ namespace App\Entity;
 use App\Repository\VinylMixRepository;
 use Doctrine\DBAL\Types\Types;
 use Doctrine\ORM\Mapping as ORM;
+use Gedmo\Mapping\Annotation\Slug;
 use Gedmo\Timestampable\Traits\TimestampableEntity;
 
 #[ORM\Entity(repositoryClass: VinylMixRepository::class)]
@@ -32,6 +33,10 @@ class VinylMix
 
     #[ORM\Column]
     private int $votes = 0;
+
+    #[ORM\Column(length: 100, unique: true)]
+    #[Slug(fields:  ['title'])]
+    private ?string $slug = null;
 
 
     public function getId(): ?int
@@ -118,5 +123,17 @@ class VinylMix
         return sprintf('https://picsum.photos/id/%d/%d',
             ($this->getId() + 50) % 1000 , $width
         );
+    }
+
+    public function getSlug(): ?string
+    {
+        return $this->slug;
+    }
+
+    public function setSlug(string $slug): self
+    {
+        $this->slug = $slug;
+
+        return $this;
     }
 }
